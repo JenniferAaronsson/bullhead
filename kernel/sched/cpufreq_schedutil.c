4a99b248ee826c1729ad3c9262eb410211eedf34
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-04-11 00:20:41 +0200

    BACKPORT: cpufreq: schedutil: Use policy-dependent transition delays
    Make the schedutil governor take the initial (default) value of the
    rate_limit_us sysfs attribute from the (new) transition_delay_us
    policy parameter (to be set by the scaling driver).
    
    That will allow scaling drivers to make schedutil use smaller default
    values of rate_limit_us and reduce the default average time interval
    between consecutive frequency changes.
    
    Make intel_pstate set transition_delay_us to 500.
    
    BACKPORT: Modified to support the separate up_rate_limit_us and
    down_rate_limit_us (upstream just has a single rate_limit_us). Also
    dropped the changes for intel_pstate as there's a merge conflict.
    
    Change-Id: I62a8543879a4d8582cdcb31ebd55607705d1c8b1
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    (cherry picked from commit 1b72e7fd304639f1cd49d1e11955c4974936d88c)
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -677,12 +677,19 @@ static void get_tunables_data(struct sugov_tunables *tunables,
 	}
 
 initialize:
-	tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-	tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
-	lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
-	if (lat) {
-		tunables->up_rate_limit_us *= lat;
-		tunables->down_rate_limit_us *= lat;
+	if (policy->up_transition_delay_us && policy->down_transition_delay_us) {
+		tunables->up_rate_limit_us = policy->up_transition_delay_us;
+		tunables->down_rate_limit_us = policy->down_transition_delay_us;
+	} else {
+		unsigned int lat;
+
+                tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
+                tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
+		if (lat) {
+                        tunables->up_rate_limit_us *= lat;
+                        tunables->down_rate_limit_us *= lat;
+                }
 	}
 	
 	pr_debug("tunables data initialized for cpu[%u]\n", cpu);
